9b4143c0a4caf39ec3391606e36483ddfc347e5d
adapter-certification-eng <adapter-certification-eng@indexexchange.com>
2018-04-19 12:11:50 -0400

    V2 internal

--- yieldbot-htb-validator.js
+++ yieldbot-htb-validator.js
@@ -10,7 +10,6 @@
  * prior written permission of Index Exchange.
  */
 
-
 'use strict';
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -23,29 +22,39 @@ var Inspector = require('../../../libs/external/schema-inspector.js');
 // Main ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-
-/* =============================================================================
- * STEP 0 | Config Validation
- * -----------------------------------------------------------------------------
- * This file contains the necessary validation for the partner configuration.
- * This validation will be performed on the partner specific configuration object
- * that is passed into the wrapper. The wrapper uses an outside library called
- * schema-insepctor to perform the validation. Information about it can be found here:
- * https://atinux.fr/schema-inspector/.
- */
 var partnerValidator = function (configs) {
     var result = Inspector.validate({
         type: 'object',
         properties: {
+            publisherId: {
+                type: 'object',
+                properties: {
+                    '*': {
+                        type: 'string',
+                        minLength: 1
+                    }
+                }
+            },
             xSlots: {
                 type: 'object',
                 properties: {
                     '*': {
                         type: 'object',
                         properties: {
-                            placementId: {
+                            adSlotId: {
                                 type: 'string',
                                 minLength: 1
+                            },
+                            sizes: {
+                                type: 'array',
+                                minLength: 1,
+                                items: {
+                                    type: 'array',
+                                    exactLength: 2,
+                                    items: {
+                                        type: 'integer'
+                                    }
+                                }
                             }
                         }
                     }
